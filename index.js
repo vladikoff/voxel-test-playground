3f961a93be7d577366870aa998ce43c907e0e358
vladikoff <vlad.filippov@gmail.com>
2013-04-15 18:13:19 -0400

    create block demo

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var player = require('voxel-player')
 var texturePath = require('painterly-textures')(__dirname)
 var voxel = require('voxel')
 var extend = require('extend')
+var fly = require('voxel-fly')
 
-module.exports = function(opts, setup) {
+
+
+module.exports = function (opts, setup) {
   setup = setup || defaultSetup
   var defaults = {
-    generate: voxel.generator['Valley'],
-    chunkDistance: 2,
+    generate: function(x,y,z) {
+      if (y === -1) { return 1;}
+    },
+    chunkDistance: 1,
+    chunkSize: 16,
     materials: [
       ['grass', 'dirt', 'grass_dirt'],
       'obsidian',
@@ -17,6 +23,7 @@ module.exports = function(opts, setup) {
       'grass',
       'plank'
     ],
+    generateChunks: true,
     texturePath: texturePath,
     worldOrigin: [0, 0, 0],
     controls: { discreteFire: true }
@@ -29,7 +36,7 @@ module.exports = function(opts, setup) {
   window.game = game // for debugging
   game.appendTo(container)
   if (game.notCapable()) return game
-  
+
   var createPlayer = player(game)
 
   // create the player from a minecraft skin file and tell the
@@ -37,9 +44,23 @@ module.exports = function(opts, setup) {
   var avatar = createPlayer(opts.playerSkin || 'player.png')
   avatar.possess()
   avatar.yaw.position.set(2, 14, 4)
-
+  var makeFly = fly(game)
+  makeFly(avatar)
   setup(game, avatar)
-  
+
+
+  var tower = [
+    1, 2, 1, 3, 1, 2, 3, 1, 2, 1, 2, 3, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1,
+    1, 2, 1, 3, 1, 2, 3, 1, 2, 1, 2, 3, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1,
+    1, 2, 1, 3, 1, 2, 3, 1, 2, 1, 2, 3, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1,
+    1, 2, 1, 3, 1, 2, 3, 1, 2, 1, 2, 3, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1
+  ]
+  tower.forEach(function (item, i) {
+    var pos = [1,i,2]
+    console.log(pos)
+    game.setBlock(pos, item)
+  });
+
   return game
 
 }
@@ -48,10 +69,18 @@ function defaultSetup(game, avatar) {
   // highlight blocks when you look at them, hold <Ctrl> for block placement
   var blockPosPlace, blockPosErase
   var hl = game.highlighter = highlight(game, { color: 0xff0000 })
-  hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
-  hl.on('remove', function (voxelPos) { blockPosErase = null })
-  hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
-  hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null })
+  hl.on('highlight', function (voxelPos) {
+    blockPosErase = voxelPos
+  })
+  hl.on('remove', function (voxelPos) {
+    blockPosErase = null
+  })
+  hl.on('highlight-adjacent', function (voxelPos) {
+    blockPosPlace = voxelPos
+  })
+  hl.on('remove-adjacent', function (voxelPos) {
+    blockPosPlace = null
+  })
 
   // toggle between first and third person modes
   window.addEventListener('keydown', function (ev) {
